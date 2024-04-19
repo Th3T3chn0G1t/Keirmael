ede7096cb30d50220fdfe1b5586da375a0bcd0bb
Th3T3chn0G1t <prs.ttg+gh@pm.me>
2024-04-19 23:21:45 +0100

    kernel: add more descriptive interrupt exception stub

--- keirmael/kernel/arch/x86_64/interrupt.c
+++ keirmael/kernel/arch/x86_64/interrupt.c
@@ -3,6 +3,8 @@
 
 #include <kml/io.h>
 
+#include <kmlk/kernel.h>
+
 #include <kmlk/arch/x86_64/ports.h>
 #include <kmlk/arch/x86_64/vectors.h>
 #include <kmlk/arch/x86_64/interrupt.h>
@@ -10,9 +12,32 @@
 struct kmlk_interrupt_frame kmlk_exception_frame;
 
 [[gnu::no_caller_saved_registers]] void kmlk_interrupt_stub(void) {
-	kml_dputf(
-			"received interrupt $X\n",
-			(kml_u64_t) kmlk_exception_frame.vector);
+	kml_u64_t vec = kmlk_exception_frame.vector;
+
+	switch(vec) {
+		case KMLK_EXC_PAGE_FAULT: {
+			kmlk_register_t cr2;
+			asm("movq %%cr2, %[cr2]" : [cr2] "=a" (cr2) :: );
+			KML_DLOG("page fault @ linear address $X", cr2);
+
+			struct kmlk_page_error* pe =
+					&kmlk_exception_frame.error_code.page_fault;
+
+			if(pe->present) {
+				const char* op = pe->write ? "write" : "read";
+				if(pe->instruction_fetch) op = "exec";
+
+				KML_DLOG("page protection violation on $S", op);
+			}
+			else KML_DLOG("fault on missing page");
+
+			break;
+		}
+
+		default: KML_DLOG("received interrupt $X", vec);
+	}
+
+	kmlk_hang();
 }
 
 void kmlk_set_interrupt(void) {
